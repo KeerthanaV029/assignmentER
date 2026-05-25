86aa93d0f7e6d16b3d850d6ee54f19e04e4cf668
Keerthana Venkatesan <keerthanavenkatesan@Keerthanas-MacBook-Air.local>
2026-05-25 13:35:37 +0530

    Configured springboot-level1 branch

--- PayrollSpringApp/src/main/java/com/app/Exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.app.Exception;
-
-public class ResourceNotFoundException extends RuntimeException
-{
-    public ResourceNotFoundException(String message)
-    {
-        super(message);
-    }
-}
