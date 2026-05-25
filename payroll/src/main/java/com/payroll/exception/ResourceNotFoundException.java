aa25dc5c7321eb1097b7c09f6402097f29c1092b
Keerthana Venkatesan <keerthanavenkatesan@Keerthanas-MacBook-Air.local>
2026-05-25 13:33:13 +0530

    Configured jdbc-project branch

--- payroll/src/main/java/com/payroll/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.payroll.exception;
-
-public class ResourceNotFoundException extends RuntimeException
-{
-    public ResourceNotFoundException(String message)
-    {
-        super(message);
-    }
-}
